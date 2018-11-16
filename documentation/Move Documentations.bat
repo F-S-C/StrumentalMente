4e55eecefec6d5d1b52c3dd4607aed5006a2754f
espositoandrea <esposito_andrea99@hotmail.com>
2018-11-16 10:00:01 +0100

    Uploaded documentations template

---
+++ documentation/Move Documentations.bat
@@ -0,0 +1,12 @@
+@echo off
+title Move Documentations
+echo I will move all PDF files to this directory. 
+pause
+cd src
+for /R %%G in (*.PDF) do (
+	echo Moving %%G
+	REM substitute move with copy to copy files
+	move %%G "../"
+)
+echo Done.
+pause
---
+++ documentation/Remove Auxiliary Files.bat
@@ -0,0 +1,14 @@
+@echo off
+title Remove Auxiliary Files
+echo I will remove all auxiliary file in all subfolders of /src. 
+pause
+cd src
+for /R %%G in (.) do (
+	echo Cleaning: %%G
+	pushd %%G
+	rm -f *.aux *.glo *.idx *.log *.toc *.ist *.acn *.acr *.alg *.bbl *.blg *.dvi *.glg *.gls *.ilg *.ind *.lof *.lot *.maf *.mtc *.mtc1 *.out *.synctex.gz
+	popd
+)
+
+echo Done.
+pause
