510d70d1cd09fd109576f4b89b47cb2bb30dcc15
espositoandrea <esposito_andrea99@hotmail.com>
2018-11-30 16:28:03 +0100

    Modificati i generatori
    Modificati i file .bat che automatizzano la generazione dellle documentazioni, accorpando tutti i file BAT in un unico file che permette, tramite un apposito menu, di scegliere quale sottoprogramma eseguire.

--- documentation/GenerateDocumentations.bat
+++ documentation/GenerateDocumentations.bat
@@ -1,23 +1,81 @@
 @echo off
-title Generate Documentations
-echo I will generate all PDF files. 
-pause
+title Generatore
+
+:menu
+cls
+echo Questo file BAT permette di eseguire automaticamente diverse operazioni.
+echo Scegliere quella da eseguire inserendo il numero dell'operazione da effettuare.
+echo MENU:
+echo      1. Generare tutti i documenti
+echo      2. Rimuovere tutti i file generati da MiKTeX a esclusione del PDF
+echo      3. Spostare tutti i documenti alla cartella "documentation"
+echo      4. Tutte le operazioni precedenti
+echo      5. Esci
+
+:choiceInput
+set /p choice="Inserire cosa si vuole fare: "
+if /I "%choice%" EQU "1" goto generateDocs
+if /I "%choice%" EQU "2" goto removeFiles
+if /I "%choice%" EQU "3" goto moveFiles
+if /I "%choice%" EQU "4" goto doEverything
+if /I "%choice%" EQU "5" goto exit
+echo La scelta inserita non puo' essere accettata.
+goto choiceInput
+
+:doEverything
+echo Eseguo tutte le operazioni elencate nel menu in ordine.
+goto generateDocs
+
+:generateDocs
+echo Genero la documentazione.
 cd src
 
 cd Pianificazione
-pdflatex Pianificazione.tex
+echo Genero la pianificazione...
+pdflatex Pianificazione.tex -interaction=nonstopmode
+cls
 
 cd ../Progettazione
-pdflatex Progettazione.tex
+echo Genero la progettazione...
+pdflatex Progettazione.tex -interaction=nonstopmode
+cls
 
 cd ..
-pdflatex DocumentazioneCompleta.tex
+echo Genero la documentazione completa...
+pdflatex DocumentazioneCompleta.tex -interaction=nonstopmode
+cls
 
 cd ..
-echo Done.
-cls
-call RemoveAuxiliaryFiles.bat
-cls
-call MoveDocumentations.bat
-cls
+echo Fatto.
 pause
+if /I "%choice%" EQU "1" goto menu
+
+:removeFiles
+echo Rimuovo tutti i file inutili dalla sottocartella /src.
+cd src
+for /R %%G in (.) do (
+	echo Cleaning: %%G
+	pushd %%G
+	rm -f *.aux *.glo *.idx *.log *.toc *.ist *.acn *.acr *.alg *.bbl *.blg *.dvi *.glg *.gls *.ilg *.ind *.lof *.lot *.maf *.mtc *.mtc1 *.out *.synctex.gz "*.synctex(busy)" *.thm
+	popd
+)
+cd ..
+echo Fatto.
+pause
+if /I "%choice%" EQU "2" goto menu
+
+:moveFiles
+echo Sposto tutte le documentazioni nella cartella corrente.
+cd src
+for /R %%G in (*.PDF) do (
+	echo Sposto %%G
+	REM substitute move with copy to copy files
+	move %%G "../"
+)
+cd ..
+echo Fatto.
+pause
+if /I "%choice%" EQU "3" goto menu
+if /I "%choice%" EQU "4" goto menu
+
+:exit
