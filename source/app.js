ca8a9e25719fc8a96a269e7622a3ace9b3a0bc27
espositoandrea <esposito_andrea99@hotmail.com>
2019-01-20 20:03:46 +0100

    Modifiche allo scheletro dell'app

--- source/app.js
+++ source/app.js
@@ -10,33 +10,39 @@ function openChildWindow(pageUrl) {
 }
 
 function createWindow() {
-    var menu = Menu.buildFromTemplate([
-        {
-            label: 'Help',
-            submenu:
-                [
-                    {
-                        label: "About",
-                        click: () => { openChildWindow("about.html"); }
-                    },
-                    {
-                        label: "Help",
-                        accelerator: "F11",
-                        click: () => { openChildWindow("help.html"); }
-                    },
-                    {
-                        label: "Exit",
-                        accelerator: "Esc",
-                        click: () => { app.quit(); }
-                    }
-                ]
-        }
-    ]);
+    // Uncomment the following lines to define a new menu
+    //
+    // var menu = Menu.buildFromTemplate([
+    //     {
+    //         label: 'Help',
+    //         submenu:
+    //             [
+    //                 {
+    //                     label: "About",
+    //                     click: () => { openChildWindow("about.html"); }
+    //                 },
+    //                 {
+    //                     label: "Help",
+    //                     accelerator: "F11",
+    //                     click: () => { openChildWindow("help.html"); }
+    //                 },
+    //                 {
+    //                     label: "Exit",
+    //                     accelerator: "Esc",
+    //                     click: () => { app.quit(); }
+    //                 }
+    //             ]
+    //     }
+    // ]);
 
-    Menu.setApplicationMenu(menu);
+    // Menu.setApplicationMenu(menu);
 
     win = new BrowserWindow({ width: 800, height: 600, show: false, icon: "assets/icon.ico" });
     win.loadFile("index.html");
+
+    // Comment out if a menu is needed
+    win.setMenu(null);
+    
     win.on("ready-to-show", () => { win.maximize(); win.show(); });
     win.on("closed", () => {
         win = null;
