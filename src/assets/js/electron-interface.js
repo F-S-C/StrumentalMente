d940d69c5fbcf4beeaa3059bb8a329ffb407b966
espositoandrea <esposito_andrea99@hotmail.com>
2019-02-05 16:02:26 +0100

    Aggiunta shortcut per avviare l'applicazione
    Come richiesto da @ax3lfernus e @prewarning.

--- src/assets/js/electron-interface.js
+++ src/assets/js/electron-interface.js
@@ -3,11 +3,22 @@ function openInBrowser(link) {
 }
 
 function openModal(content, windowIcon = "./assets/icon.ico") {
-    const remote = require('electron').remote;
+    const remote = require("electron").remote;
     var win = new remote.BrowserWindow({ width: 800, height: 600, parent: remote.getCurrentWindow(), modal: true, icon: windowIcon, frame: false });
     win.setMenu(null);
     if (/^(f|ht)tp(s?):\/\//i.test(content))
         win.loadURL(content);
     else
         win.loadFile(content);
+}
+
+function openOnKeyboardShortcut(shortcut, content, openInSameWindow = true) {
+    const remote = require("electron").remote;
+    const electronLocalshortcut = require('electron-localshortcut');
+    electronLocalshortcut.register(remote.getCurrentWindow(), shortcut, () => {
+        if (/^(f|ht)tp(s?):\/\//i.test(content))
+            remote.getCurrentWindow().loadURL(content);
+        else
+            remote.getCurrentWindow().loadFile(content);
+    });
 }
