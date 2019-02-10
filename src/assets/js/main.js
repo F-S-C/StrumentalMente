7aaceb8b160c47ef1064653d73187888a65832c9
Ax3lFernus <a.annese99@gmail.com>
2019-02-10 17:26:03 +0100

    Aggiunta funzione di transizione delle slide
    - Corretto errore nel template "contenuto_generico.html"
    - Aggiunto bottone "quiz" nel template "contenuto generico"
    - Inserita funzione di transizione delle slide
    - Aggiunto lo stile per i bottoni disattivati .btn e .btn-outlined
    - Aggiunte variabili grey e grey-dark nei fogli di stile CSS
    - Aggiunto foglio di stile "section.css" che contiene le animazioni CSS per il tag section
    
    N.B. Nel template si può evitare di inserire le classi per nascondere gli elementi dato che è presente una funzione che, al caricamento della pagina, si occupa di inserire . Sono state tuttavia inserite tali attributi.
    Tuttavia è consigliato inserirli manualmente per evitare di mostrare, per alcune frazioni di secondo, tutti gli elementi nella pagina prima che essa subica la modifica dalla suddetta funzione.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,3 +19,60 @@ function drop(name) {
     else
         el.className = "";
 }
+
+/* Funzioni di transizione delle sezioni in una pagina */
+
+/*
+Funzione che, al caricamento della pagina, si occupa di impostare il numero 
+di tag section presenti all'interno della pagina nella memoria locale del browser, di
+impostare come sezione visibile corrente la prima (sempre all'interno della memoria locale)
+e di nascondere tutti i tag section successivi al primo.
+*/
+function numberOfSection() {
+    sessionStorage.setItem("number_of_elements", document.getElementsByTagName("section").length);
+    sessionStorage.setItem("curr_element", "0");
+    document.getElementById("back").disabled = true;
+    if (document.getElementsByTagName("section").length < 2) {
+        document.getElementById("next").style.display = "none";
+        document.getElementById("quiz").style.display = "inline-block";
+    }
+    else {
+        for (var i = 1; i < document.getElementsByTagName('section').length; i++)
+            document.getElementsByTagName("section")[i].className = "hide";
+        document.getElementById("quiz").style.display = "none";
+    }
+}
+
+/*
+La funzione, in base al valore assunto da slide (true/false) cambia la sezione corrente in quella precedente (in caso di slide = false)
+o in quella successiva (in caso di slide = true). Inoltre si occupa di aggiornare il numero della slide corrente nella memoria temporanea
+del browser. Inpltre, in base al numero di slide, si occupa di rendere visibili (o nascondere) i relativi pulsanti di spostamento
+(avanti con id next, indietro con id back e quiz con id quiz).
+*/
+function changeWindow(slide) {
+    var curr_element = sessionStorage.getItem("curr_element");
+    var number_of_elements = sessionStorage.getItem("number_of_elements") - 1;
+    document.getElementsByTagName("section")[curr_element].className = "hide";
+    if (slide) {
+        if (number_of_elements > curr_element)
+            curr_element++;
+        if (number_of_elements == curr_element) {
+            document.getElementById("next").style.display = "none";
+            document.getElementById("quiz").style.display = "inline-block";
+        }
+    }
+    else {
+        if (curr_element == number_of_elements) {
+            document.getElementById("next").style.display = "inline-block";
+            document.getElementById("quiz").style.display = "none";
+        }
+        if (number_of_elements >= curr_element)
+            curr_element--;
+    }
+    if (curr_element == 0)
+        document.getElementById("back").disabled = true;
+    else
+        document.getElementById("back").disabled = false;
+    setTimeout(function () { document.getElementsByTagName("section")[curr_element].className = "show" }, 100);
+    sessionStorage.setItem("curr_element", curr_element);
+}
