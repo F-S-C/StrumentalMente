56e1894dce87e567eb2e21162182a4e3414d265a
espositoandrea <esposito_andrea99@hotmail.com>
2019-03-29 17:06:24 +0100

    Conversione a TypeScript (1)

--- Makefile
+++ Makefile
@@ -29,7 +29,7 @@ docs: jsdoc
 
 jsdoc:
 	if not exist ".\docs\src\Realizzazione\code" mkdir .\docs\src\Realizzazione\code
-	jsdoc2md .\src\app.js .\src\assets\js\main.js .\src\assets\js\render.js .\src\assets\js\quiz.js .\src\assets\js\accordi_basso.js .\src\assets\js\accordi_chitarra.js -d 1 > .\docs\src\Realizzazione\code\JsDocumentation.md
+	jsdoc2md .\src\app.ts .\src\StrumentalMente.ts .\src\assets\js\main.js .\src\assets\js\render.js .\src\assets\js\quiz.js .\src\assets\js\accordi_basso.js .\src\assets\js\accordi_chitarra.js -d 1 > .\docs\src\Realizzazione\code\JsDocumentation.md
 	cd .\docs\src\Realizzazione\code && pandoc JsDocumentation.md -f markdown -t html -o JsDocumentation.html
 	cd .\docs\src\Realizzazione\code && pandoc JsDocumentation.html -f html -t latex -o JsDocumentation.tex
 	cd .\docs\src\Realizzazione\code && del JsDocumentation.html
---
+++ src/app.ts
@@ -0,0 +1,45 @@
+/**
+ * @file app.ts
+ *
+ * È il file principale dell'applicazione. 
+ *
+ * Utilizza la classe Main definita in StrumentalMente.js per far funzionare
+ * l'applicazione.
+ */
+
+import { app, BrowserWindow } from 'electron';
+import Main from './StrumentalMente';
+
+Main.main(app, BrowserWindow);
+
+(function () {
+	// Include this at the very top of both your main and window processes, so that
+	// it loads as soon as possible.
+	//
+	// Why does this work? The node.js module system calls fs.realpathSync a _lot_
+	// to load stuff, which in turn, has to call fs.lstatSync a _lot_. While you
+	// generally can't cache stat() results because they change behind your back
+	// (i.e. another program opens a file, then you stat it, the stats change),
+	// caching it for a very short period of time is :ok: :gem:. These effects are
+	// especially apparent on Windows, where stat() is far more expensive - stat()
+	// calls often take more time in the perf graph than actually evaluating the
+	// code!!
+
+	// npm install lru-cache first
+	const LRU = require('lru-cache');
+	var lru = new LRU({ max: 256, maxAge: 250/*ms*/ });
+
+	var fs = require('fs');
+	var origLstat = fs.lstatSync.bind(fs);
+
+	// NB: The biggest offender of thrashing lstatSync is the node module system
+	// itself, which we can't get into via any sane means.
+	fs.lstatSync = function (p) {
+		let r = lru.get(p);
+		if (r) return r;
+
+		r = origLstat(p);
+		lru.set(p, r);
+		return r;
+	};
+})();
